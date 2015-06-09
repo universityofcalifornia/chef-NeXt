f0caa4e4db2d598480b3567a328812de8a949f57
Sandon Jurowski <jurowski@gmail.com>
2015-06-08 21:33:42 -0700

    updated revision of NeXt to 1.0.04-dev-54-gdb84464

--- roles/next-app.rb
+++ roles/next-app.rb
@@ -11,6 +11,8 @@ run_list([
   'recipe[next::app]'
 ])
 
+# last revision was 1.0.01-dev
+# updated to 1.0.04-dev-54-gdb84464 on 20150608
 default_attributes({
   'elasticsearch' => {
     'version' => '1.3.4',
@@ -23,7 +25,7 @@ default_attributes({
   },
   'next' => {
     'app' => {
-      'revision' => '1.0.01-dev',
+      'revision' => '1.0.04-dev-54-gdb84464',
       'server_port' => 443,
       'environment' => {
         'oauth2' => {
