7c3ad2e63457387d0389d88a839f7bd34af64233
Steve Nolen <technolengy@gmail.com>
2015-07-01 08:18:58 -0700

    specify mysql_service provider due to chef 12.4 regression. only supports rhel6. see https://github.com/chef-cookbooks/mysql/issues/359

--- cookbooks/next/recipes/mysql_server.rb
+++ cookbooks/next/recipes/mysql_server.rb
@@ -3,4 +3,5 @@ include_recipe 'next::mysql'
 mysql_service 'default' do
   initial_root_password node['next']['mysql']['root']['password']
   action [:create, :start]
+  provider Chef::Provider::MysqlService::Sysvinit
 end
