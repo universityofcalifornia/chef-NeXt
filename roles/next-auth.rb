4a9e9ff8eb2f722f1511cb959a67ed041b0b5557
Steve Nolen <technolengy@gmail.com>
2015-07-24 09:31:36 -0700

    add attribute-map. comment out base config items in hash since we are using a file resource now

--- roles/next-auth.rb
+++ roles/next-auth.rb
@@ -24,14 +24,14 @@ default_attributes({
     }
   },
   'shibboleth-sp' => {
-    'configuration' => {
-      'application_defaults' => {
-        'REMOTE_USER' => 'SHIB_eduPersonPrincipalName',
-        'session_initiator' => {
-          'entityID' => 'urn:mace:incommon:ucla.edu' # TODO: replace with real initiator
-        }
-      }
-    },
+    #'configuration' => {
+    #  'application_defaults' => {
+    #    'REMOTE_USER' => 'SHIB_eduPersonPrincipalName',
+    #    'session_initiator' => {
+    #      'entityID' => 'urn:mace:incommon:ucla.edu' # TODO: replace with real initiator
+    #    }
+    #  }
+    #},
     'attribute-map' => {
       'urn:oid:1.3.6.1.4.1.5923.1.1.1.6' => {
         'id' => 'SHIB_EPPN',
@@ -39,22 +39,28 @@ default_attributes({
           'type' => 'ScopedAttributeDecoder'
         }
       },
-      'urn:mace:dir:attribute-def:eduPersonAffiliation' => {
+      'urn:oid:1.3.6.1.4.1.5923.1.1.1.10' => 'SHIB_EDUPERSONTARGETEDID',
+      'urn:oid:1.3.6.1.4.1.5923.1.1.1.7' => 'entitlement',
+      'urn:oid:1.3.6.1.4.1.5923.1.1.1.1' => {
         'id' => 'SHIB_EDU_PERSON_AFFILIATION',
         'decoder' => {
+          'type' => 'StringAttributeDecoder',
           'caseSensitive' => 'false'
         }
       },
-      'urn:oid:1.3.6.1.4.1.5923.1.1.1.10' => {
-        'id' => 'SHIB_EDUPERSONTARGETEDID',
+      'urn:oid:1.3.6.1.4.1.5923.1.1.1.9' => {
+        'id' => 'scopedAffiliation',
         'decoder' => {
-            'type' => 'NameIDAttributeDecoder',
-            'formatter' => '$Name'
+          'type' => 'ScopedAttributeDecoder',
+          'caseSensitive' => 'false'
         }
       },
-      'urn:mace:dir:attribute-def:givenName' => 'SHIB_GIVEN_NAME',
-      'urn:mace:dir:attribute-def:sn' => 'SHIB_SN',
-      'urn:mace:dir:attribute-def:mail' => 'SHIB_MAIL'
+      'urn:oid:2.5.4.4' => 'SHIB_SN',
+      'urn:oid:2.5.4.42' => "SHIB_GIVEN_NAME",
+      'urn:oid:2.16.840.1.113730.3.1.241' => 'displayName',
+      'urn:oid:0.9.2342.19200300.100.1.3' => 'SHIB_MAIL',
+      'urn:oid:2.16.840.1.113916.1.1.4.1' => 'UCnetID',
+      'urn:oid:2.16.840.1.113916.1.1.5' => 'UCTrustAssurance'
     }
   },
   'php' => {
