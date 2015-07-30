15fb43f09c1f5f9bff3a4b14d2acb161b01c2d37
Steve Nolen <technolengy@gmail.com>
2015-07-30 11:52:45 -0700

    rename template file to match recipe

--- cookbooks/next/metadata.rb
+++ cookbooks/next/metadata.rb
@@ -1,10 +1,11 @@
 name 'next'
 license 'BSD'
-version '1.0.3'
+version '1.0.4'
 maintainer 'Eric Bollens'
 maintainer_email 'eric@eb.io'
 
 recipe 'next::app', 'Installs, configures and runs the NeXt application'
+recipe 'next::proxy', 'Sets apache2 as a reverse proxy for the NeXt app'
 recipe 'next::https_rewrite', 'Sets a redirect to HTTPS based on port rules'
 recipe 'next::iptables', 'Sets IP tables rules per NeXt-namespaced attribute'
 recipe 'next::mysql', 'Defines an initial MySQL root password'
