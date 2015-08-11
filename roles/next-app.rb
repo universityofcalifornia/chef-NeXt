a606fbdd879501fa39788982dcc9cbd6b97b6da6
Steve Nolen <technolengy@gmail.com>
2015-08-11 14:53:07 -0700

    bump version

--- roles/next-app.rb
+++ roles/next-app.rb
@@ -24,7 +24,7 @@ default_attributes({
   },
   'next' => {
     'app' => {
-      'revision' => '1.0.18',
+      'revision' => '1.0.22',
       'server_port' => 5000,
       'environment' => {
         'oauth2' => {
